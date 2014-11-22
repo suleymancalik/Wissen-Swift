0efc7f6da4eb3a66c0ff33e1e537fd8939f17b16
Suleyman Calik <suleymancalik@gmail.com>
2014-11-22 14:27:54 +0200

    Gesture'lar eklendi

--- Samples/Ders7/Ders7/KoleksiyonCell.swift
+++ Samples/Ders7/Ders7/KoleksiyonCell.swift
@@ -12,4 +12,69 @@ class KoleksiyonCell: UICollectionViewCell {
     
     @IBOutlet var picture:UIImageView!
     
+    
+    override func awakeFromNib() {
+
+        // saga dogru swipe yapildiginda algilamak icin UISwipeGestureRecognizer
+        // olusturup view'e ekliyoruz
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: Selector("swipeRight"))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        addGestureRecognizer(swipeRight)
+        
+        
+        // sola dogru swipe yapildiginda algilamak icin UISwipeGestureRecognizer
+        // olusturup view'e ekliyoruz
+        var swipeLeft = UISwipeGestureRecognizer(target: self, action: Selector("swipeLeft"))
+        swipeLeft.direction = .Left
+        addGestureRecognizer(swipeLeft)
+    }
+    
+    
+    func swipeRight() {
+        if picture.alpha < 1.0 {
+            picture.alpha += 0.1
+        }
+    }
+    
+    
+    func swipeLeft() {
+        if picture.alpha > 0.1 {
+            picture.alpha -= 0.1
+        }
+    }
+
+    
+    // MARK: Touch Methods
+    
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        
+        var touch = touches.anyObject() as UITouch
+        var point = touch.locationInView(self)
+        
+        println("DOKUNDU: \(point)")
+        
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        var touch = touches.anyObject() as UITouch
+        var point = touch.locationInView(self)
+
+        println("HAREKET ETTIRDI: \(point)")
+    }
+    
+
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        println("BITTI")
+    }
+    
+    override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        println("CANCELLED")
+    }
+    
 }
+
+
+
+
+
