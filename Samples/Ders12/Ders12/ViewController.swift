65d429b560a885e3bbd313b8bd890237fa21d910
Suleyman Calik <suleymancalik@gmail.com>
2014-12-20 13:00:54 +0200

    AlertController eklendi

--- Samples/Ders12/Ders12/ViewController.swift
+++ Samples/Ders12/Ders12/ViewController.swift
@@ -18,17 +18,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    func openImagePicker(sourceType:UIImagePickerControllerSourceType) {
+        var picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = self
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
 
     
     //MARK: - Action Methods
     
     @IBAction func actionAddImage(sender: UIBarButtonItem) {
         
-        var picker = UIImagePickerController()
-        picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-        picker.delegate = self
-        self.presentViewController(picker, animated: true, completion: nil)
-        
+        if UIImagePickerController.isCameraDeviceAvailable(UIImagePickerControllerCameraDevice.Rear) ||
+        UIImagePickerController.isCameraDeviceAvailable(UIImagePickerControllerCameraDevice.Front) {
+            // Cihazda on yada arka kamera var mi diye bakiyoruz.
+            
+            var alert = UIAlertController(title: "Fotoğraf Ekle", message: "Kaynak Seciniz", preferredStyle: UIAlertControllerStyle.ActionSheet)
+            
+            
+            var action1 = UIAlertAction(title: "Kamera", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                self.openImagePicker(.Camera)
+            })
+            var action2 = UIAlertAction(title: "Fotograf Albumu", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                self.openImagePicker(.PhotoLibrary)
+            })
+            var cancelAction = UIAlertAction(title: "Vazgec", style: UIAlertActionStyle.Cancel, handler: nil)
+            
+            
+            alert.addAction(action1)
+            alert.addAction(action2)
+            alert.addAction(cancelAction)
+            
+            presentViewController(alert, animated: true, completion: nil)
+        }
+        else {
+            // Simulatorde burasi calisir
+            openImagePicker(UIImagePickerControllerSourceType.PhotoLibrary)
+        }
     }
     
     
@@ -44,6 +73,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         println("RESIM SECTI !!!")
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = selectedImage
+
+            UIImageWriteToSavedPhotosAlbum(selectedImage, self, "image:didFinishSavingWithError:contextInfo:", nil)
+            
+            picker.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo: UnsafePointer<()>) {
+        println("RESIM KAYDEDILDI: \(contextInfo)")
     }
     
 }
