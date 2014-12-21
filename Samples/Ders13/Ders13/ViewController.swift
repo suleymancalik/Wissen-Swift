64d69d7937616578f84d96ec986f2180ac8b5626
Suleyman Calik <suleymancalik@gmail.com>
2014-12-21 11:14:06 +0200

    Video stream eklendi

--- Samples/Ders13/Ders13/ViewController.swift
+++ Samples/Ders13/Ders13/ViewController.swift
@@ -10,24 +10,45 @@ import UIKit
 import MediaPlayer
 
 class ViewController: UIViewController {
-
+    
     var videoPlayer:MPMoviePlayerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        prepareVideoPlayer(MPMovieSourceType.File)
+        prepareVideoPlayer(MPMovieSourceType.Streaming)
+        videoPlayer.play()
     }
-
     
-    func prepareVideoPlayer() {
+    
+    func prepareVideoPlayer(sourceType:MPMovieSourceType) {
         
-        var videoURL = NSBundle.mainBundle().URLForResource("nasa", withExtension: "mp4")
+        if sourceType == MPMovieSourceType.File {
+            var videoURL = NSBundle.mainBundle().URLForResource("nasa", withExtension: "mp4")
+            videoPlayer = MPMoviePlayerController(contentURL: videoURL)
+        }
+        else {
+            var streamURL = NSURL(string: "http://www.nasa.gov/multimedia/nasatv/NTV-Public-IPS.m3u8")
+            videoPlayer = MPMoviePlayerController(contentURL: streamURL)
+        }
         
-        videoPlayer = MPMoviePlayerController(contentURL: videoURL)
-        videoPlayer.movieSourceType = MPMovieSourceType.File
+        videoPlayer.movieSourceType = sourceType
+        
         videoPlayer.view.frame = self.view.bounds
         self.view.addSubview(videoPlayer.view)
+        
+        videoPlayer.repeatMode = MPMovieRepeatMode.One
+        videoPlayer.controlStyle = MPMovieControlStyle.None
+        videoPlayer.scalingMode = MPMovieScalingMode.AspectFit
     }
-
+    
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        videoPlayer.view.frame.size = size
+    }
+    
+    
 }
 
 
