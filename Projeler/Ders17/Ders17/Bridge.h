f4f20a1f0682d6598a945a08617bea399b6de2f4
Suleyman Calik <suleymancalik@gmail.com>
2015-01-03 11:41:49 +0200

    Ders17 eklendi

---
+++ Projeler/Ders17/Ders17/Bridge.h
@@ -0,0 +1,10 @@
+//
+//  Bridge.h
+//  Ders17
+//
+//  Created by Suleyman Calik on 03/01/15.
+//  Copyright (c) 2015 Wissen. All rights reserved.
+//
+
+#import "AFNetworking.h"
+
---
+++ Projeler/Ders17/Ders17/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ders17
+//
+//  Created by Suleyman Calik on 03/01/15.
+//  Copyright (c) 2015 Wissen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
