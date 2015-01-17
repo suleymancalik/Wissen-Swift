b49af5b5e275204ec0d2f64ca47f862d8092a91c
Suleyman Calik <suleymancalik@gmail.com>
2015-01-17 12:28:18 +0200

    Sayma islemi yapildi

--- Projeler/Ders21/Ders21/ViewController.swift
+++ Projeler/Ders21/Ders21/ViewController.swift
@@ -10,18 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var userDefaults = NSUserDefaults.standardUserDefaults()
+    let keySayac = "sayac"
     
     @IBOutlet weak var lblCount: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        count = userDefaults.integerForKey(keySayac)
+        lblCount.text = "\(count)"
     }
+    
     
     
     //MARK: - Action Methods
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        if motion == UIEventSubtype.MotionShake {
+            count = 0
+            userDefaults.setInteger(count, forKey: keySayac)
+            userDefaults.synchronize()
+            lblCount.text = "\(count)"
+        }
+    }
+
 
     @IBAction func btnCountTapped(sender: UIButton) {
+        count++
+        userDefaults.setInteger(count, forKey: keySayac)
+        userDefaults.synchronize()
+        lblCount.text = "\(count)"
     }
 
 }
+
+
+
+
+
 
