481d30f994688dae18590e9e7f2acddfa138c0ea
Suleyman Calik <suleymancalik@gmail.com>
2015-01-17 11:46:41 +0200

    Ders21 eklendi

---
+++ Projeler/Ders21/Ders21/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ders21
+//
+//  Created by Suleyman Calik on 17/01/15.
+//  Copyright (c) 2015 Wissen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var lblCount: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: - Action Methods
+
+    @IBAction func btnCountTapped(sender: UIButton) {
+    }
+
+}
+
