99e96f3d397d1725a7c8979d2d1d2edd2f5e58c0
Suleyman Calik <suleymancalik@gmail.com>
2015-01-18 12:35:59 +0200

    Paging tamamlandi

--- Projeler/Ders22/Ders22/ViewController.swift
+++ Projeler/Ders22/Ders22/ViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 
-class ViewController:UIViewController, UITableViewDataSource {
+class ViewController:UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView:UITableView!
     
     var numbers = [Dictionary<String,String>]()
     var currentPage = 1
+    var allDataFetched = false
+    var fetchingData = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +26,16 @@ class ViewController:UIViewController, UITableViewDataSource {
     
     func getNextNumbers() {
         
+        if allDataFetched || fetchingData {
+            return
+        }
+        
         var urlString = "http://cop4425.parseapp.com/\(currentPage).json"
         if let url = NSURL(string: urlString) {
             
             var request = NSURLRequest(URL: url)
             
+            fetchingData = true
             NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
                 
                 if error == nil {
@@ -41,19 +48,21 @@ class ViewController:UIViewController, UITableViewDataSource {
                         
                         if numbersArray!.count < 10 {
                             // verilerin sonuna geldi
-                            // TODO
+                            self.allDataFetched = true
                         }
                         else {
                             // sunucuda hala veri var
-                            // TODO
+                            self.currentPage++
                         }
                         
-                        
+                        // Gelen icerikleri self.numbers'a ekliyoruz
                         for number in numbersArray! {
                             self.numbers.append(number)
                         }
                         
+                        // Tabloyu refresh ediyoruz
                         self.tableView.reloadData()
+                        self.fetchingData = false
                         
                     }
                     else {
@@ -79,23 +88,50 @@ class ViewController:UIViewController, UITableViewDataSource {
     //MARK: - TableView Methods
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numbers.count
+        return allDataFetched ? numbers.count : (numbers.count + 1)
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCellWithIdentifier("DataCell") as UITableViewCell
         
-        var dict = numbers[indexPath.row]
+        // indexPath.row numbers dizisininden buyukse WaitingCell koyuyoruz
+        if indexPath.row >= numbers.count {
+            var cell = tableView.dequeueReusableCellWithIdentifier("WaitingCell") as UITableViewCell
+            return cell
+        }
+        else {
+            var cell = tableView.dequeueReusableCellWithIdentifier("DataCell") as UITableViewCell
+            
+            var dict = numbers[indexPath.row]
+            
+            var title = dict["title"]
+            var description = dict["description"]
+            
+            cell.textLabel?.text = title
+            cell.detailTextLabel?.text = description
+            
+            return cell
+        }
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
         
-        var title = dict["title"]
-        var description = dict["description"]
+        if indexPath.row == numbers.count - 3 {
+            getNextNumbers()
+        }
         
-        cell.textLabel?.text = title
-        cell.detailTextLabel?.text = description
-        
-        return cell
+        if cell.reuseIdentifier == "WaitingCell" {
+            if let spinner = cell.contentView.viewWithTag(10) as? UIActivityIndicatorView {
+                spinner.startAnimating()
+            }
+        }
     }
 }
+
+
+
+
 
 
 
